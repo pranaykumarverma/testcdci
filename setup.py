c102df886f7c86a1a1ec3ffe85e83b882e44a703
pranaykumarverma <pranaykumarverma@gmail.com>
2021-11-21 19:02:57 +0530

    Merging with Wafer Code

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="docker-heroku",
+    name="wafer-detection",
     version="0.0.3",
     description="ML project",
-    author="Avnish yadav", 
+    author="PVERMA",
     packages=find_packages(),
     license="MIT"
 )
