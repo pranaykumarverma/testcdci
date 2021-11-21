5c2d75e88c7c64033dd5b5ffe344cce97dc941f1
pranaykumarverma <pranaykumarverma@gmail.com>
2021-11-21 19:22:50 +0530

    Syntax error in main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successful!!")
 
- port = int(os.getenv("PORT",5000))
+port = int(os.getenv("PORT",5000))
 if __name__ == "__main__":
     host = '0.0.0.0'
     #port = 5000
